4e1caa041f2a660b3c0d6d1ef66dc4735810d8a9
Kirill Novik <kino6052@colorado.edu>
2019-11-24 20:55:09 -0700

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
---
+++ src/todo-service.test.tsx
@@ -0,0 +1,1 @@
+describe('Todo Service', () => {});
