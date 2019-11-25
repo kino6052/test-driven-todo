263e83e1564332c656261637f4a265fa0409585c
Kirill Novik <kino6052@colorado.edu>
2019-11-24 22:32:46 -0700

    Make Test Pass

--- src/todo-presentation.test.tsx
+++ src/todo-presentation.test.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
 import uniqueId from "lodash.uniqueid";
 import { Todo } from "./todo-service";
+import Enzyme, { mount } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const TodoComponent: React.SFC<{ todo: Todo }> = props => (
+  <div>{props.todo.text}</div>
+);
+
+const hasText = (text: string, wrapper: Enzyme.ReactWrapper) => {
+  return wrapper.findWhere(w => w.text() === text).first().exists();
+};
 
 describe("Display Todo", () => {
   it("should display todo name", () => {
@@ -9,6 +22,6 @@ describe("Display Todo", () => {
       checked: false
     };
     const wrapper = mount(<TodoComponent todo={todo} />);
-    expect(hasText("first", wrapper)).toBe(true);
+    expect(hasText(todo.text, wrapper)).toBe(true);
   });
 });
