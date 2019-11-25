3df3a1a1be261da1df8b1789fb26f0d7d48d92aa
Kirill Novik <kino6052@colorado.edu>
2019-11-24 23:12:12 -0700

    Make Tests Pass

--- src/integration.test.tsx
+++ src/integration.test.tsx
@@ -3,6 +3,29 @@ import { BehaviorSubject } from "rxjs";
 
 const subject = new BehaviorSubject<Todo[]>([]);
 
+const onCheckboxClick = (id: string, subject: BehaviorSubject<Todo[]>) => {
+  const state = subject.getValue();
+  const todo = state.find(td => td.id === id);
+  if (todo) subject.next(TodoService.update(TodoService.toggleCompleteTodo(todo), state));
+};
+
+const getCheckedArray = () => subject.getValue().map(td => td.checked);
+
+const onAddNewTodoSubmit = (text: string, subject: BehaviorSubject<Todo[]>) => {
+  const state = subject.getValue();
+  subject.next(TodoService.addTodo(text, state));
+};
+
+const getTextArray = () => subject.getValue().map(td => td.text);
+
+const getTodos = async (dependencies: { todoEndpoint: () => Promise<Todo[]> }) => {
+  try {
+    return await dependencies.todoEndpoint();
+  } catch (e) {
+    return [];
+  }
+};
+
 describe("On Click", () => {
   it("should change state on click", () => {
     const state = TodoService.addTodo("2", TodoService.addTodo("1", []));
