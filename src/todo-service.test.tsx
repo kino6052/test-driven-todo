2d50f7d6687be3a6e3a00bbd378527c431d786eb
Kirill Novik <kino6052@colorado.edu>
2019-11-24 21:09:37 -0700

    Add a Failing Test

--- src/todo-service.test.tsx
+++ src/todo-service.test.tsx
@@ -1,1 +1,17 @@
-describe('Todo Service', () => {});
+interface Todo {
+  id: string,
+  text: string,
+  checked: boolean,
+};
+
+const addTodo = () => {
+  // TODO: Implement
+};
+
+describe('Todo Service', () => {
+  it('should add todo to an empty list', () => {
+    const todos: Todo[] = [];
+    const result = addTodo('test', todos);
+    expect(result.map(td => td.text)).toEqual(['test']);
+  });
+});
