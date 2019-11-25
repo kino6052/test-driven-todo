6ae757c05594b5a9221db7ec125bb046983ec48a
Kirill Novik <kino6052@colorado.edu>
2019-11-24 21:15:16 -0700

    Make Test Pass

--- src/todo-service.test.tsx
+++ src/todo-service.test.tsx
@@ -1,11 +1,14 @@
+import uniqueId from "lodash.uniqueid";
+
 interface Todo {
   id: string,
   text: string,
   checked: boolean,
 };
 
-const addTodo = () => {
-  // TODO: Implement
+const addTodo = (text: string, todos: Todo[]) => {
+  const newTodos = todos.map(td => td);
+  return [...newTodos, { id: uniqueId(), text, checked: false }];
 };
 
 describe('Todo Service', () => {
