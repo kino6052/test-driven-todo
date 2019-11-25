f6a0a34f7541bccb84c3fcb189bcc8fc0a1d71b2
Kirill Novik <kino6052@colorado.edu>
2019-11-24 23:00:03 -0700

    Make Tests Pass

--- src/todo-presentation.test.tsx
+++ src/todo-presentation.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import uniqueId from "lodash.uniqueid";
 import { Todo, TodoService } from "./todo-service";
 import Enzyme, { mount } from "enzyme";
@@ -7,11 +7,46 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 const TodoComponent: React.SFC<{ todo: Todo }> = props => (
-  <div>{props.todo.text}</div>
+  <Fragment>
+    <div>{props.todo.text}</div>
+    <Checkbox checked={props.todo.checked}/>
+  </Fragment>
 );
 
 const hasText = (text: string, wrapper: Enzyme.ReactWrapper) => {
   return wrapper.findWhere(w => w.text() === text).first().exists();
+};
+
+const contains = function <T>(Component: React.SFC<T>, wrapper: Enzyme.ReactWrapper) {
+  return wrapper.find(Component).first().exists();
+};
+
+const Checkbox: React.SFC<{ checked: boolean }> = props => (
+  <div className={props.checked ? "checked" : ""}></div>
+);
+
+const isChecked = (wrapper: Enzyme.ReactWrapper) => (
+  wrapper
+    .findWhere(w => w.hasClass("checked"))
+    .first()
+    .exists()
+);
+
+const TodoList: React.SFC<{ todos: Todo[] }> = props => (
+  <Fragment>
+    {props.todos.map(td => (
+      <TodoComponent todo={td} />
+    ))}
+  </Fragment>
+);
+
+const howMany = function<T>(
+  Component: React.SFC<T>,
+  wrapper: Enzyme.ReactWrapper
+) {
+  return wrapper
+    .find(Component)
+    .length
 };
 
 describe("Display Todo", () => {
@@ -40,7 +75,7 @@ describe("Display Todo", () => {
       checked: true
     };
     const wrapper = mount(<TodoComponent todo={todo} />);
-    expect(isChecked(TodoComponent, wrapper)).toBe(true);
+    expect(isChecked(wrapper)).toBe(true);
   });
   it("should have a checkbox unchecked if todo completion is false", () => {
     const todo: Todo = {
@@ -49,7 +84,7 @@ describe("Display Todo", () => {
       checked: false
     };
     const wrapper = mount(<TodoComponent todo={todo} />);
-    expect(isChecked(TodoComponent, wrapper)).toBe(false);
+    expect(isChecked(wrapper)).toBe(false);
   });
   it("should display a list of todos", () => {
     const todos: Todo[] = TodoService.addTodo(
